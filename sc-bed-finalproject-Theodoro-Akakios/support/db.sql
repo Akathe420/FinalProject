c8d83055268d0dcbed43a7083ed57accf79064f4
Akathe420 <theodoro_akakios@yahoo.com>
2024-10-09 02:36:30 +0200

    Add files via upload

---
+++ sc-bed-finalproject-Theodoro-Akakios/support/db.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE IF NOT EXISTS kahuna;
+
+USE kahuna;
+
+CREATE TABLE IF NOT EXISTS Product(
+    id                  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    serial              VARCHAR(255) NOT NULL,
+    name                VARCHAR(255) NOT NULL,
+    warrantyLength      INT(11) NOT NULL
+);
+
+-- DELETE FROM Product WHERE id = 1;
+
+CREATE TABLE IF NOT EXISTS User(
+    id                 INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstName          VARCHAR(55)  NOT NULL,
+    lastName           VARCHAR(55)  NOT NULL,
+    email              VARCHAR(100) NOT NULL,
+    accessLevel        CHAR(10)     NOT NULL DEFAULT 'user',
+    phone              VARCHAR(20)  NULL    ,
+    address            VARCHAR(100) NULL    
+);
+
+CREATE TABLE IF NOT EXISTS AccessToken(
+    id              INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId          INT NOT NULL,
+    token           VARCHAR(255) NOT NULL,
+    birth           TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT c_accesstoken_user
+        FOREIGN KEY(userId) REFERENCES User(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Registration(
+    id               INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userId           INT      NOT NULL,
+    productId        INT      NOT NULL,
+    registrationDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT c_registration_user FOREIGN KEY (userId) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT c_registration_product FOREIGN KEY (productId) REFERENCES Product(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- INSERT INTO User (firstName, lastName, email, password)
+-- VALUES ('Theo', 'Akakios', 'theo@test.com', 'abc123def456');
